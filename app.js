685566fe53fccf8c0df923db72983ccf02df35ca
Alex Maldonado <alex.maldonado@outlook.com>
2022-03-24 00:31:54 -0300

    Se crea primera versión de la interfaz de consola - Mostrar menú de opciones

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-require('colors');
+const { showMenu, pausa } = require('./helpers/mensajes');
+console.clear();
+
+
+const main = async () => {
+    console.log('Hola Mundo');
+    let answer = '';
+
+    do {
+        answer = await showMenu();
+        console.log({answer});
+        if (answer !== '0') await pausa();
+    } while (answer !== '0');
+    // await pausa();
+}
+
+
+main();
